933befe703ffefed4482bed0a047f6606b1410a4
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-03-01 12:20:40 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+let intervalId;
+const startRandomColor = () => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    console.log(getRandomHexColor());
+  }, 1000);
+  startBtn.disabled = true;
+};
+
+const stopRandomColor = () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', startRandomColor);
+stopBtn.addEventListener('click', stopRandomColor);
